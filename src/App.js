d527491f6a401b71802e5e7f8c46ed2ea900b7bc
Meerim <meerione11@gmail.com>
2022-03-21 22:09:39 +0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header/Header"
+import BlogEntries from "./Components/BlogEntries/BlogEntries";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Footer from "./Components/Footer/Footer";
+import "./index.css"
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+      <div className="w3-content" style={{maxWidth:"1400px"}}>
+            <Header/>
+            <div className="w3-row">
+            <BlogEntries/>
+            <Sidebar/>
+            </div>
+      </div>
+    <Footer/>
+    </>
+  )
 }
 
 export default App;
+
+
---
+++ src/Components/Tags/Tags.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {tags} from "../../constant";
+
+const Tags = () => {
+    return (
+        <div className="w3-card w3-margin">
+            <div className="w3-container w3-padding">
+                <h4>Tags</h4>
+            </div>
+            <div className="w3-container w3-white">
+                   <p/><span className="w3-tag w3-black w3-margin-bottom" style={{marginRight:"3px"}}>Travel</span>
+                       {
+                           tags.map((item)=>{
+                               console.log(tags);
+                               return <span className="w3-tag w3-light-grey w3-small w3-margin-bottom" style={{marginRight:"3px"}}>{item.tag}</span>
+
+                    })
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Tags;
+
+
